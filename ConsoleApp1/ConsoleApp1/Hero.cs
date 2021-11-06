141a64722fe6acce37d2cee4f1e2924c613cee97
Serhiyenka Volha <volhaws@mail.ru>
2021-11-06 11:34:24 +0300

    Init commit

---
+++ ConsoleApp1/ConsoleApp1/Hero.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Hero
+    {
+        private int x;
+        private int y;
+        private int xP;
+        private int yP;
+        private int heath;
+        private int steps;
+        private bool endGame;
+
+        public Hero(int x, int y, int xP, int yP)
+        { this.x = x;
+          this.y = y;
+          this.xP = xP;
+          this.yP = yP;
+          heath = 10;
+          steps = 10;
+          endGame = false;
+        }
+
+        public int X
+        {
+            set
+            {
+                x = value;
+            }
+            get
+            {
+                return x;
+            }
+        }
+        public int XP
+        {
+            set
+            {
+                x = value;
+            }
+            get
+            {
+                return xP;
+            }
+        }
+        public int Y
+        {
+            set
+            {
+                y = value;
+            }
+            get
+            {
+                return y;
+            }
+        }
+        public int YP
+        {
+            set
+            {
+                y = value;
+            }
+            get
+            {
+                return yP;
+            }
+        }
+
+        public void Explode()
+        {
+            var rand = new Random();
+            int bombPower = rand.Next(11);
+            heath -= bombPower;
+            if (heath <= 0)
+                EndGame();
+
+        }
+
+        public void Step(char[,] arr, int n)
+        {
+            steps--;
+            if (steps == 0)
+            { EndGame();
+                return;
+            }
+
+            if (arr[x, y] == 'x')
+            {
+                Explode();
+
+            }
+
+
+        }
+
+        public void EndGame()
+        {
+            steps=0;
+            heath = 0;
+            endGame = true;
+
+        }
+
+        public void StartGame()
+        {
+            steps = 10;
+            heath = 10;
+            endGame = false;
+
+        }
+
+        public void InfoPrint()
+        {
+            Console.Write($" moves left {steps} ,");
+            Console.WriteLine($" health left {heath}");
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            
+            char[,] matrixMap = new char[n, n];
+            char[,] matrixDisplay = new char[n, n];
+
+            Hero player = new Hero(0,0,0,0);
+            player= kartGenerate(matrixMap, matrixDisplay, n);
+
+            Print2DArray(matrixDisplay, n);
+            Print2DArray(matrixMap, n);
+            
+            ConsoleKeyInfo key;
+            do
+            {
+                
+                key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.LeftArrow)
+                {
+                    int x, y;
+                    if (player.X != 0)
+                    { 
+                        x = player.X--;
+                        player.step();
+
+                    }
+                }
+                   player.InfoPrint();
+            }
+            while (key.Key != ConsoleKey.Escape);
+            
+
+        }// end main
+
+
+
+
+
+
+// PrintmatrixMapMap(matrixMap);
+        public static void Print2DArray(char[,] arr, int n)
+        {
+            Console.Write("\n");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    //Random rnd = new Random();
+                    //int chislo = rnd.Next(-10, 10);
+                    Console.Write(arr[j, i]);
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public static void PrintmatrixMap(Array a)
+        {
+            for (int i = 0; i < a.GetLength(0); ++i)
+            {
+                for (int j = 0; i < a.GetLength(1); ++i)
+                    Console.Write("\t" + a.GetValue(i,j));
+                Console.WriteLine();
+
+
+                //foreach (char[] x in a)
+                //{
+                //    foreach (char y in x)
+                //        Console.Write("\t " + y);
+                //    Console.WriteLine();
+                //}
+            }
+
+
+
+        }
+
+        public static Hero kartGenerate(char[,] matrixMap, char[,] matrixDisplay, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrixMap[j, i] = '*';
+                    matrixDisplay[j, i] = '*';
+                }
+            }
+
+            //  
+
+            var rand = new Random();
+
+            int count = 0;
+            int HeroCoordinate = rand.Next(4);// генеруруем номер угла карты
+            int PrincessCoordinate;
+            int PlayerX = 0, PlayerY = 0, PincessX = 0, PrincessY = 0;
+            while (true)// генеруруем номер угла карты отличный от героя
+            {
+                PrincessCoordinate = rand.Next(4);
+                if (PrincessCoordinate != HeroCoordinate)
+                    break;
+            }
+
+            switch (HeroCoordinate)//наносим на карту  героя
+            {
+                case 0:
+                    matrixMap[0, 0] = 'H'; matrixDisplay[0, 0] = 'H'; PlayerX = 0; PlayerY = 0; break;
+                case 1:
+                    matrixMap[9, 0] = 'H'; matrixDisplay[9, 0] = 'H'; PlayerX = 9; PlayerY = 0; break;
+                case 2:
+                    matrixMap[0, 9] = 'H'; matrixDisplay[0, 9] = 'H'; PlayerX = 0; PlayerY = 9; break;
+                case 3:
+                    matrixMap[9, 9] = 'H'; matrixDisplay[9, 9] = 'H'; PlayerX = 9; PlayerY = 9; break;
+
+            }
+
+            switch (PrincessCoordinate)// наносим на карту принцессу
+            {
+                case 0:
+                    matrixMap[0, 0] = 'P'; matrixDisplay[0, 0] = 'P'; PincessX = 0; PrincessY = 0; break;
+                case 1:
+                    matrixMap[9, 0] = 'P'; matrixDisplay[9, 0] = 'P'; PincessX = 9; PrincessY = 0; break;
+                case 2:
+                    matrixMap[0, 9] = 'P'; matrixDisplay[0, 9] = 'P'; PincessX = 0; PrincessY = 9; break;
+                case 3:
+                    matrixMap[9, 9] = 'P'; matrixDisplay[9, 9] = 'P'; PincessX = 9; PrincessY = 9; break;
+
+            }
+
+            Hero player = new Hero(PlayerX, PlayerY, PincessX, PrincessY);
+
+            while (count < 10) // расставляем 10 ловушек
+            {
+                int temp = rand.Next(100);
+                int NumRow = temp / 10;
+                int NumCol = temp % 10;
+                if (matrixMap[NumRow, NumCol] == '*')
+                {
+                    matrixMap[NumRow, NumCol] = 'x';
+                    count++;
+                }
+
+
+            }
+            return player;
+        }
+
+        
+
+
+
+
+
+
+    }
+}
+
+
+    
